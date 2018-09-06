b2f60c313e6df99ec5853f755c744067e98463af
roba <paul.roba@wisebiz-group.com>
2018-09-06 19:03:45 +0300

    Also add post headers

--- app/src/main/java/com/rp/hitta/api/HittaSandboxAPI.kt
+++ app/src/main/java/com/rp/hitta/api/HittaSandboxAPI.kt
@@ -4,12 +4,19 @@ import com.rp.hitta.model.Review
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 /**
  * Created by roba
  */
 interface HittaSandboxAPI {
+    @Headers(
+            "Content-Type: application/x-www-form-urlencoded",
+            "Charset: UTF-8",
+            "X-HITTA-DEVICE-NAME: MOBILE_WEB",
+            "X-HITTA-SHARED-IDENTIFIER: 15188693697264027"
+    )
     @POST("/reviews/v1/company")
     @FormUrlEncoded
     fun persistReview(@Field("score") score: Int,
